449479d64f9edd0914e66b8c83c6aead102e8770
Иванов Иван <BROTON07@yandex.ru>
2022-09-17 12:49:12 +0400

    не работает бд

---
+++ ServiceStationProgram/ServiceStationViewMaster/SparesChangeWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using ServiceStationContracts.BindingModels;
+using ServiceStationContracts.BusinessLogicsContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ServiceStationViewMaster
+{
+    /// <summary>
+    /// Логика взаимодействия для SparesChangeWindow.xaml
+    /// </summary>
+    public partial class SparesChangeWindow : Window
+    {
+        public int Id { set { id = value; } }
+        private readonly ISparesLogic _logic;
+        private int? id;
+        public SparesChangeWindow(ISparesLogic logic)
+        {
+            InitializeComponent();
+            _logic = logic;
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxName.Text))
+            {
+                MessageBox.Show("Заполните название", "Ошибка", MessageBoxButton.OK,
+               MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                _logic.CreateOrUpdate(new SparesBindingModel
+                {
+                    Id = id,
+                    Name = textBoxName.Text,
+                    MasterId = App.Master.Id
+                });
+                MessageBox.Show("Сохранение прошло успешно", "Сообщение",
+               MessageBoxButton.OK, MessageBoxImage.Information);
+                DialogResult = true;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK,
+               MessageBoxImage.Error);
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (id.HasValue)
+            {
+                try
+                {
+                    var view = _logic.Read(new SparesBindingModel { Id = id })?[0];
+                    if (view != null)
+                    {
+                        textBoxName.Text = view.Name;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK,
+                   MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
---
+++ ServiceStationProgram/ServiceStationViewMaster/SparesWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using ServiceStationContracts.BindingModels;
+using ServiceStationContracts.BusinessLogicsContracts;
+using ServiceStationContracts.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Unity;
+
+namespace ServiceStationViewMaster
+{
+    /// <summary>
+    /// Логика взаимодействия для SparesWindow.xaml
+    /// </summary>
+    public partial class SparesWindow : Window
+    {
+        private readonly ISparesLogic _logic;
+        public SparesWindow(ISparesLogic logic)
+        {
+            InitializeComponent();
+            _logic = logic;
+        }
+
+        private void buttonChange_Click(object sender, EventArgs e)
+        {
+            if (dataGrid.SelectedItems.Count == 1)
+            {
+                var form = App.Container.Resolve<SparesChangeWindow>();
+                form.Id = Convert.ToInt32(((SparesViewModel)dataGrid.SelectedItem).Id);
+                if (form.ShowDialog() == true)
+                {
+                    LoadData();
+                }
+            }
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGrid.SelectedItems.Count == 1)
+            {
+                if (MessageBox.Show("Удалить запись", "Вопрос", MessageBoxButton.YesNo,
+               MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    int id =
+                   Convert.ToInt32(((SparesViewModel)dataGrid.SelectedItem).Id);
+                    try
+                    {
+                        _logic.Delete(new SparesBindingModel { Id = id });
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK,
+                       MessageBoxImage.Error);
+                    }
+                    LoadData();
+                }
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+        private void LoadData()
+        {
+            try
+            {
+                var list = _logic.Read(new SparesBindingModel { MasterId = App.Master.Id });
+                if (list != null)
+                {
+                    dataGrid.ItemsSource = list;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK,
+               MessageBoxImage.Error);
+            }
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+            var form = App.Container.Resolve<SparesChangeWindow>();
+            if (form.ShowDialog() == true)
+            {
+                LoadData();
+            }
+
+        }
+    }
+}
